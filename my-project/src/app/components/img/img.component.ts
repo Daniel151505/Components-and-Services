f9d2879a181896ccde0c96d9192361631aeaa1bb
Daniel151505 <daniel150520011@gmail.com>
2021-12-30 13:38:16 -0500

    Component life cycle

--- my-project/src/app/components/img/img.component.ts
+++ my-project/src/app/components/img/img.component.ts
@@ -11,9 +11,34 @@ export class ImgComponent implements OnInit {
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/default.png';
 
-  constructor() { }
+  constructor() {
+    // before render
+    // No async -- once time
+    console.log('constructor', 'imgValue =>', this.img);
+    this.img = 'asas'
+  }
+
+  ngOnChanges() {
+    // before -- duranting render
+    // changes inputs -- times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
 
   ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
+
+  ngAfterViewInit(): void {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    // delete
+    console.log('ngOnDestroy');
   }
 
   imgError() {
