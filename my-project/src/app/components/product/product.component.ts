6fa90534c5afc7609bdf7abf74038ae40a0506e5
Daniel151505 <daniel150520011@gmail.com>
2021-12-30 13:25:57 -0500

    Component for product

---
+++ my-project/src/app/components/product/product.component.ts
@@ -0,0 +1,23 @@
+import { Product } from './../../models/product.model';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() product: Product = {
+    id: '',
+    price: 0,
+    image: '',
+    name: ''
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
